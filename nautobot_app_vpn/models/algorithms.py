4b8dfe26fb93fd004dd2aa643ce81aa4372e4da4
narendra.polisetty <npolisetty26@gmail.com>
2025-07-31 10:28:01 +0000

    Add migration for algorithms and new algorithms.py module

---
+++ nautobot_app_vpn/models/algorithms.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+class EncryptionAlgorithm(models.Model):
+    code = models.CharField(max_length=32, unique=True)
+    label = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.label
+
+class AuthenticationAlgorithm(models.Model):
+    code = models.CharField(max_length=32, unique=True)
+    label = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.label
+
+class DiffieHellmanGroup(models.Model):
+    code = models.CharField(max_length=16, unique=True)
+    label = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.label
